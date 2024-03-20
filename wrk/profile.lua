ddb043e2fdcb29e051bbe8c2060f17b2b8f7eaf7
zsz <2492127354@qq.com>
2024-03-21 00:32:22 +0800

    压测

---
+++ wrk/profile.lua
@@ -0,0 +1,5 @@
+wrk.method="GET"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["User-Agent"] = "PostmanRuntime/7.32.3"
+wrk.headers["Authorization"]="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTA5NTM5NDQsIlVpZCI6MSwiVXNlckFnZW50IjoiUG9zdG1hblJ1bnRpbWUvNy4zNy4wIn0.657MiAzGC3uUGGpwyEYm_2_9LRVsVUqA5k6GsNN5nod8lyfIvbSRa10_ra01cTJ7KU_Xw4upqq3mGpCFcREfMQ
+"
