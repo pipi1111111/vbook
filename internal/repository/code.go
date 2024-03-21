88cc2d29f1b21ecd604ff06bf2af223e27182649
zsz <2492127354@qq.com>
2024-03-21 21:53:29 +0800

    压测

---
+++ internal/repository/code.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"context"
+	"vbook/internal/repository/cache"
+)
+
+var ErrCodeVerifyTooMany = cache.ErrCodeVerifyTooMany
+
+type CodeRepository interface {
+	Set(ctx context.Context, biz string, phone string, code string) error
+	Verify(ctx context.Context, biz, phone, code string) (bool, error)
+}
+type codeRepository struct {
+	cache cache.CodeCache
+}
+
+func NewCodeRepository(cache cache.CodeCache) CodeRepository {
+	return &codeRepository{
+		cache: cache,
+	}
+}
+func (c *codeRepository) Set(ctx context.Context, biz string, phone string, code string) error {
+	return c.cache.Set(ctx, biz, phone, code)
+}
+func (c *codeRepository) Verify(ctx context.Context, biz, phone, code string) (bool, error) {
+	return c.cache.Verify(ctx, biz, phone, code)
+}
---
+++ internal/service/sms/localsms/service.go
@@ -0,0 +1,17 @@
+package localsms
+
+import (
+	"context"
+	"log"
+)
+
+type Service struct {
+}
+
+func NewService() *Service {
+	return &Service{}
+}
+func (s *Service) Send(ctx context.Context, tpId string, args []string, numbers ...string) error {
+	log.Println("验证码是:", args)
+	return nil
+}
